4de47ac555c92b353216c0a1383ebac3768917e5
Rodrigo Luiz Madeira Furlaneti <rodrigofurlaneti31@hotmail.com>
2026-08-03 10:50:54 -0300

    Added entity Users

---
+++ 2-Backend/Source/BioScore.Core/Common/Auth/IJwtTokenGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioScore.Core.Common.Auth
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(User user);
+    }
+}
